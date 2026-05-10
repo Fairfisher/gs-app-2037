be3ef7bfeab5b9f7a0dc387d7e9570eac45701ad
H_D <78508831+Fairfisher@users.noreply.github.com>
2026-05-10 19:19:15 +0200

    feat: add lib/widgets/bottom_navigation.dart

---
+++ lib/widgets/bottom_navigation.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class BottomNavigation extends StatelessWidget {
+  final int currentIndex;
+
+  const BottomNavigation({
+    super.key,
+    required this.currentIndex,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationBar(
+      selectedIndex: currentIndex,
+      onDestinationSelected: (int index) {
+        switch (index) {
+          case 0:
+            context.go('/');
+            break;
+          case 1:
+            context.go('/activity');
+            break;
+          case 2:
+            context.go('/stats');
+            break;
+          case 3:
+            context.go('/profile');
+            break;
+        }
+      },
+      destinations: const [
+        NavigationDestination(
+          icon: Icon(Icons.home_outlined),
+          selectedIcon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.fitness_center_outlined),
+          selectedIcon: Icon(Icons.fitness_center),
+          label: 'Activity',
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.bar_chart_outlined),
+          selectedIcon: Icon(Icons.bar_chart),
+          label: 'Stats',
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.person_outline),
+          selectedIcon: Icon(Icons.person),
+          label: 'Me',
+        ),
+      ],
+    );
+  }
+}
